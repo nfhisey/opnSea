d860a72354476015e34136e9c7a341b42c572183
Nikki <97553125+nfhisey@users.noreply.github.com>
2022-02-10 01:05:50 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace programC
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = web.Load("https://opensea.io/collection/rtfkt-mnlth");
+            foreach (var item in doc.DocumentNode.SelectNodes("//div[@class='Overflowreact__OverflowContainer-sc-7qr9y8-0 jPSCbX']"))
+            {
+                Console.WriteLine(item.InnerText);
+            }
+
+
+        }
+    }
+
+
+}
